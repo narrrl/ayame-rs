a39cfd8f232ddfeaafe30c95508b7f68f4b73533
Nils Pukropp <nils@nirusu.codes>
2021-09-24 00:15:00 +0200

    updated run script

--- run.sh
+++ run.sh
@@ -3,23 +3,43 @@
 ABSPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 FILE="nirust"
 PIDF="nirust.pid"
+CONFIG="config.toml"
 
-cd $ABSPATH
-if [ "$1" == "start" ]; then
-	if [[ -f "$PIDF" ]]; then
-		printf "Nirust already running\n"
+function main() {
+	if [ "$1" == "start" ]; then
+		if [[ -f "$PIDF" ]]; then
+			printf "Nirust already running\n"
+			exit -1
+		fi
+		if [[ ! -f "$FILE" ]]; then
+			cargo build --release
+			cp ./target/release/nirust .
+		fi
+		./nirust & echo $! > ./nirust.pid
+	elif [ "$1" == "stop" ]; then
+		kill -SIGINT "$(cat $PIDF)"
+		rm $PIDF
+	else
+		printf "No argument provided\n"
 		exit -1
 	fi
-	if [[ ! -f "$FILE" ]]; then
-		cargo build --release
-		cp ./target/release/nirust .
+}
+
+function check_config() {
+	if [[ ! -f "$CONFIG" ]]; then
+		printf "Your bot token: "
+		read TOKEN
+		echo "token = \"$TOKEN\"" > $CONFIG
+		printf "\nYour application id (usually your bot user id): "
+		read APP_ID
+		echo "application_id = $APP_ID" >> $CONFIG
+		printf "\nYour prefix: "
+		read PREFIX
+		echo "prefix = \"$PREFIX\"" >> $CONFIG
 	fi
-	./nirust & echo $! > ./nirust.pid
-elif [ "$1" == "stop" ]; then
-	kill -SIGINT "$(cat $PIDF)"
-	rm $PIDF
-else
-	printf "No argument provided\n"
-	exit -1
-fi
-exit 0
+
+}
+
+cd $ABSPATH
+check_config
+main $1
