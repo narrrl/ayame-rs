dbb6e851018ec459c4aad63e442a6ed2dda99671
Nils Pukropp <nils@nirusu.codes>
2021-11-15 04:21:29 +0100

    updated version

--- src/database.rs
+++ src/database.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, PartialEq, Eq, Clone)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq, Clone)]
 struct GuildBase {
     guild_id: u64,
     // TODO: need to reimplement command dispatching
